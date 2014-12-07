333238f4fbeb21eabbbc2e4c8c53999d1b257c30
m-e-h <info@martyhelmick.com>
2014-12-07 17:50:09 -0500

    add read more

--- content/search.php
+++ content/search.php
@@ -14,7 +14,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php kit_byline(); ?>
+			<?php kit_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -24,6 +24,6 @@
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php kit_post_footer(); ?>
+		<?php kit_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
